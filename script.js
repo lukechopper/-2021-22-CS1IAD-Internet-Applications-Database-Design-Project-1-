56594cf185534368361e6f1d469efcf675f843d7
lukechopper <lukechopper22@gmail.com>
2022-02-10 12:38:43 +0000

    Making Better

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const headerHamburger = mainHeader.getElementsByClassName('main-header__hamburge
  * The <ul> containing the different options of the main header. E.g., home, projects, contact me, etc.
  */
 const headerList = mainHeader.getElementsByClassName('main-header__list')[0];
-
 /**
  * sets our main header to animate into minimised mode if we are scrolled far down enough. Sets it to do the reverse of this 
    if we have scrolled up to the top.
@@ -69,7 +68,23 @@ const sortOutMainMenu = (function() {
  */
 addEventListener('scroll', sortOutMainMenu);
 addEventListener('load', sortOutMainMenu);
-addEventListener('resize', sortOutMainMenu);
+const windowResize = (function(){
+    let windowState = '';
+    function setWindowState(){
+        if(window.innerWidth < 576){
+            windowState = 'smallest';
+        }else if(window.innerWidth > 575){
+            windowState = 'small';
+        }
+    }; setWindowState();
+    
+    return function(){
+        sortOutMainMenu();
+        let oldWindowState = windowState;
+        setWindowState();
+    }
+})();
+addEventListener('resize', windowResize);
 
 /**
  * With the relevant animation, opens and closes the hamburger drop-down menu, as from our main header, when the hamburger icon
@@ -84,14 +99,20 @@ const configureHamburgerHeaderList = (function(){
     let isOpen = false;
     return function(){
         isOpen = !isOpen;
+        headerList.classList.remove('main-header__list--hamburger-small-open');
         if(isOpen){
             if(window.innerWidth < 576){
-                gsap.to(headerList, {height: '159px', duration: 0.25, ease: 'power1'});
+                animateHeaderHamburger('open_small', 0.25);
+                gsap.to(headerList, {height: '195px', duration: 0.25, ease: 'power1', onComplete: function(){
+                    headerList.classList.add('main-header__list--hamburger-small-open');
+                }});
                 return;
             }
-            gsap.to(headerList, {height: '165px', duration: 0.25, ease: 'power1'});
+            animateHeaderHamburger('open_large', 0.25);
+            gsap.to(headerList, {height: '224px', duration: 0.25, ease: 'power1'});
             return;
         }
+        animateHeaderHamburger('close', 0.25);
         gsap.to(headerList, {height: '0px', duration: 0.25, ease: 'power1'});
     }
 })();
@@ -100,3 +121,52 @@ const configureHamburgerHeaderList = (function(){
    clicked on.
  */
 headerHamburger.onclick = configureHamburgerHeaderList;
+/**
+ * Animates the header hamburger icon open and closed for 2 different media queries, hence why there are 3 different animations in total. 
+   This only animates the icon and not the drop-down menu associated with it.
+ * @returns void
+ */
+function animateHeaderHamburger(type, duration){
+    if(type === 'open_small'){
+        gsap.timeline({defaults: {duration: duration, ease: 'power1'}})
+                .to(headerHamburger.getElementsByClassName('main-header__hamburger_top')[0], {
+                    transform: 'rotate(-45deg) translate(-10px, 16px)'
+                }, 0)
+                .to(headerHamburger.getElementsByClassName('main-header__hamburger_bottom')[0], {
+                    transform: 'rotate(45deg) translate(-8px, -16px)'
+                }, 0)
+                .to(headerHamburger.getElementsByClassName('main-header__hamburger_center')[0], {
+                    opacity: '0'
+                }, 0);
+        headerHamburger.setAttribute('open', 'true');
+        return;
+    }
+    if(type === 'open_large'){
+        gsap.timeline({defaults: {duration: duration, ease: 'power1'}})
+                .to(headerHamburger.getElementsByClassName('main-header__hamburger_top')[0], {
+                    transform: 'rotate(-45deg) translate(-15px, 16px)'
+                }, 0)
+                .to(headerHamburger.getElementsByClassName('main-header__hamburger_bottom')[0], {
+                    transform: 'rotate(45deg) translate(-15px, -16px)'
+                }, 0)
+                .to(headerHamburger.getElementsByClassName('main-header__hamburger_center')[0], {
+                    opacity: '0'
+                }, 0);
+        headerHamburger.setAttribute('open', 'true');
+        return;
+    }
+    if(type === 'close'){
+        gsap.timeline({defaults: {duration: duration, ease: 'power1'}})
+                .to(headerHamburger.getElementsByClassName('main-header__hamburger_top')[0], {
+                    transform: 'rotate(0deg) translate(-0px, 0px)'
+                }, 0)
+                .to(headerHamburger.getElementsByClassName('main-header__hamburger_bottom')[0], {
+                    transform: 'rotate(0deg) translate(0px, -0px)'
+                }, 0)
+                .to(headerHamburger.getElementsByClassName('main-header__hamburger_center')[0], {
+                    opacity: '1'
+                }, 0);
+                headerHamburger.setAttribute('open', '');
+        return;
+    }
+}
